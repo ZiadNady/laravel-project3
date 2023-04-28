3b56d935a205a408e8d2e3c48e1b836299365d79
ziadNady <nadyziad25@gmail.com>
2023-04-28 16:14:51 +0300

    area View Updated

--- resources/views/layouts/Area/addNewCounrty.blade.php
+++ resources/views/layouts/Area/addNewCounrty.blade.php
@@ -4,26 +4,25 @@
 
 @section('content')
 
-<form>
+<form action="{{ route('countries.store') }}" method="POST" enctype="multipart/form-data">
+    @csrf
     <div class="container">
         <div class="row">
             <div class="col">
                 <div class="row">
-                    <div class="col-md-6"><label class="form-label form-label form-label">Country</label><input class="form-control form-control form-control" type="text"></div>
-                    <div class="col-md-6"><label class="form-label form-label form-label">Province</label><input class="form-control form-control form-control" type="text"></div>
+                    <div class="col-md-6">
+                        <label for="country_name" class="form-label form-label form-label">Country</label>
+                        <input class="form-control form-control form-control" type="text" name="country_name" id="country_name">
+                        @error('country_name')
+                        <div>{{ $message }}</div>
+                    @enderror
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <div class="row">
-                    <div class="col-md-6"><label class="form-label form-label form-label">City</label><input class="form-control form-control form-control" type="text"></div>
-                    <div class="col-md-6"><label class="form-label form-label form-label">District</label><input class="form-control form-control form-control" type="text"></div>
-                </div>
-            </div>
-        </div>
-        <div class="row text-center"><div><button class="btn btn-primary" type="button">Submit</button></div></div>
+        <div class="row text-center"><div><button class="btn btn-primary" type="submit">Submit</button></div></div>
     </div>
 </form>
+
 
 @endsection
